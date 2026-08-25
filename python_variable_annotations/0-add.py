64f142567101caf744b400c62708e9b0801a33b9
reem <ryma976@gmail.com>
2026-08-25 10:08:46 +0000

    0. Basic annotations - add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for task 0: Basic annotations - add."""
+
+
+def add(a: float, b: float) -> float:
+    """Add two floating-point numbers and return their sum as a float."""
+    return a + b
